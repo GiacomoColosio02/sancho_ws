d3695b7e6e87fa93b0b638c557c8ad653748fc20
Giacomo <colosio.giacomo.2002@gmail.com>
2026-04-21 13:50:24 +0200

    Add sancho_bringup launch file and YAML config

--- src/sancho_bringup/setup.py
+++ src/sancho_bringup/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'sancho_bringup'
@@ -10,12 +12,16 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.py')),
+        (os.path.join('share', package_name, 'config'),
+            glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='giacomo',
     maintainer_email='colosio.giacomo.2002@gmail.com',
-    description='TODO: Package description',
+    description='Launch files e configurazione per avviare l intero stack SANCHO',
     license='Apache-2.0',
     extras_require={
         'test': [
